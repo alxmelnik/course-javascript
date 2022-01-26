372885a7e17bccaf55f7d2c77097c451afb1b0f9
Alexsandr <alxmelnik@yandex.ru>
2022-01-27 01:54:57 +0300

    2 hw

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,15 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+
+    fn(el, i, array);
+  }
+}
+
+// forEach([1, 2, 3], (el) => console.log(el))
 
 /*
  Задание 2:
@@ -20,7 +28,19 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const doubleArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+
+    doubleArr.push(fn(el, i, array));
+  }
+
+  return doubleArr;
+}
+
+// map([1, 2, 3], (el) => console.log(el ** 2));
 
 /*
  Задание 3:
@@ -31,7 +51,18 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let sumNum = initial || array[0];
+  let i = initial ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    sumNum = fn(sumNum, array[i], i, array);
+  }
+
+  return sumNum;
+}
+
+// reduce([1, 2, 3], (all, current) => console.log(all + current))
 
 /*
  Задание 4:
@@ -41,8 +72,16 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const upperKey = [];
 
+  for (const key in obj) {
+    upperKey.push(key.toUpperCase());
+  }
+  return upperKey;
+}
+
+// console.log(upperProps({ name: 'Сергей', lastName: 'Петров' }))
 /*
  Задание 5 *:
 
